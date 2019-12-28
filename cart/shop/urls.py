8bd486be5125979bb52fd431f86586a825f7564b
Aditya Taksande <taksandeaditya@gmail.com>
2019-12-28 11:51:57 +0530

    creted new paths

--- cart/shop/urls.py
+++ cart/shop/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="Shop Home")
+    path("", views.index, name="Shop Home"),
+    path("contact/", views.contact, name="Contact Us"),
+    path("tracker/", views.tracker, name="Tracking Status"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.productView, name="Product View"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path("about/", views.about, name="About Us")
 ]
